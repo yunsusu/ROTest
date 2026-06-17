dfc0e1310f28128d0d352161169ab30d4a8f548b
yunsusu <rladbstn1212@gmail.com>
2026-06-18 00:29:43 +0900

    페이지 정상 접속 테스트

--- cypress/e2e/mainPage/load.cy.js
+++ cypress/e2e/mainPage/load.cy.js
@@ -1,9 +1,11 @@
 describe("페이지 정상 접속 테스트", () => {
-  // Given - 사용자가 메인 페이지에 접속했을 때
-  cy.visit("/");
-  // When - 페이지가 처음 로드되면
-  cy.document().its("readyState").should("eq", "complete");
-  // Then - 주요 콘텐츠가 정상적으로 표시되어야 한다
-  cy.get(".gameStart").should("be.visible");
-  cy.get(".login-btn").should("be.visible");
+  it("페이지 정상 접속 가능한지", () => {
+    // Given - 사용자가 메인 페이지에 접속했을 때
+    cy.visit("/");
+    // When - 페이지가 처음 로드되면
+    cy.document().its("readyState").should("eq", "complete");
+    // Then - 주요 콘텐츠가 정상적으로 표시되어야 한다
+    cy.get(".gameStart").should("be.visible");
+    cy.get(".login-btn").should("be.visible");
+  });
 });
